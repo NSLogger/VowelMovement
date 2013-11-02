30b9a4f5f91bc3e9c3acea1ff55989fb277b16a6
Arun Agrawal <arunagw@gmail.com>
2013-11-02 21:50:10 +0100

    Initial Commit

---
+++ VowelMovement/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  VowelMovement
+//
+//  Created by Arun Agrawal on 02/11/13.
+//  Copyright (c) 2013 Arun Agrawal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
